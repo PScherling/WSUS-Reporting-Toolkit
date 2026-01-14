da830cd89ae96ae5f2faf37f9ebea5ec68dd4b59
Patrick Scherling <scherling.pa@gmail.com>
2026-01-14 13:36:25 +0100

    Enhance script metadata and error handling
    Updated the script's metadata and improved error handling for file copying operations.

--- Source Files/manual_Install-PSC_wsus-report-tool.ps1
+++ Source Files/manual_Install-PSC_wsus-report-tool.ps1
@@ -31,9 +31,10 @@
           Contact: @Patrick Scherling
           Primary: @Patrick Scherling
           Created: 2025-05-02
-          Modified: 2025-05-09
+          Modified: 2026-01-14
 
           Version - 0.0.1 - () - Finalized functional version 1.
+		  Version - 0.0.2 - (2026-01-14) - Catching a bug in Step 1 in case you have blank spaces in the source path
           
 
           TODO:
@@ -145,9 +146,9 @@ function Start-Configuration {
 						}
 						else {
 							# If it's a file, copy it
-							Write-Host "Download file: $item.FullName to $targetItemPath"
-							Write-Log " Download file: $item.FullName to $targetItemPath"
-							Copy-Item -Path $item.FullName -Destination $targetItemPath -Force
+							Write-Host "Download file: $($item.FullName) to $targetItemPath"
+							Write-Log " Download file: $($item.FullName) to $targetItemPath"
+							Copy-Item -Path "$($item.FullName)" -Destination $targetItemPath -Force
 						}
 						
 						#Progress Bar
@@ -320,4 +321,5 @@ function Start-Configuration {
 }
 
 Start-Configuration
+
 
